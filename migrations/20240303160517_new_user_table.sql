35c25a7e3bc0785701ee5884f6009ba95e2fc84c
depatia <plekhov.egor@gmail.com>
2024-07-03 23:08:02 +0300

    update migrations

--- migrations/20240303160517_new_user_table.sql
+++ migrations/20240303160517_new_user_table.sql
@@ -1,11 +1,19 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS users
-(
-    id        INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email     VARCHAR(255) NOT NULL UNIQUE,
-    pass_hash BLOB NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `pass_hash` blob NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `lastname` varchar(20) DEFAULT NULL,
+  `middlename` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `classname` varchar(3) DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT '0',
+  `date_of_birth` varchar(10) DEFAULT NULL,
+  `permission_level` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=957 DEFAULT CHARSET=utf8mb3;
 -- +goose StatementEnd
 
 -- +goose Down
